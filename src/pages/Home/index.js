a19eaa724ccc051bba101079177b25f85f5c460c
Gabrielncosta <gabrielncosta208@gmail.com>
2020-03-16 21:39:07 -0300

    redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -19,6 +20,15 @@ export default class Home extends Component {
     }))
     this.setState({ products: data });
   }
+
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    })
+  };
 
   render() {
     const { products } = this.state;
@@ -33,7 +43,7 @@ export default class Home extends Component {
             />
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="#FFF" /> 3
               </div>
@@ -47,3 +57,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
